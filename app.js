03cbbec7548b5c11b430097dc387ceca3b2c25e2
Henggao Cai <creaturemonster@users.noreply.github.com>
2020-09-25 17:01:01 -0400

    Completed

---
+++ app.js
@@ -0,0 +1,24 @@
+const app=Vue.createApp({
+  data(){
+    return {
+      isVisible:true,
+      userClass="",
+      userColor=""
+    };
+  },
+  computed:{
+    userClasses(){
+      return{
+        [this.userClass] : Boolean(this.userClass),
+        visible:this.visible,
+        hidden:!this.visible
+      };
+    }
+  },
+  methods:{
+    toggleVisibility(){
+      this.isVisible=!this.isVisible;
+    }
+  }
+});
+app.mount("#assignment");
